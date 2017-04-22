d40da921986e023e47a2d4224872f872f088b61e
Asmadek <alma2610@ya.ru>
2017-04-22 11:21:49 +0300

    Initial structure

---
+++ app/src/main/java/com/example/aleksei/allrightreader/BookManager/BookManager.java
@@ -0,0 +1,8 @@
+package com.example.aleksei.allrightreader.BookManager;
+
+/**
+ * Created by asmadek on 22.04.17.
+ */
+
+public class BookManager {
+}
---
+++ app/src/main/java/com/example/aleksei/allrightreader/FileManager/FileManager.java
@@ -0,0 +1,8 @@
+package com.example.aleksei.allrightreader.FileManager;
+
+/**
+ * Created by asmadek on 22.04.17.
+ */
+
+public class FileManager {
+}
